c607d54226a38ecd528c83e96e5a343f42fcafa6
Kovács Lóránt <kovacs.lorant@itk.ppke.hu>
2021-05-10 10:14:56 +0200

    Update test.py

--- code/test.py
+++ code/test.py
@@ -6,7 +6,7 @@ TEST_TXT_PATH= '/home/lorant/Projects/data/SceneChangeDet/cd2014/test.txt'
 test_data = dates.Dataset(cfg.VAL_DATA_PATH,cfg.VAL_LABEL_PATH,
                             TEST_TXT_PATH,'val',transform=False)
 test_dataloader = Data.DataLoader(test_data, batch_size= cfg.BATCH_SIZE,
-                                shuffle= False, num_workers= 4, pin_memory= True)
+                                shuffle= False, num_workers= 1, pin_memory= True)
 
 base_seg_model = 'deeplab'
 
@@ -28,7 +28,7 @@ for batch_idx, batch in enumerate(test_dataloader):
     inputs1,input2,targets = inputs1.cuda(),input2.cuda(), targets.cuda()
     inputs1,inputs2,targets = Variable(inputs1, volatile=True),Variable(input2,volatile=True) ,Variable(targets)
     
-    out_conv5,out_fc,out_embedding = model(inputs1,inputs2)
+    out_conv5,out_fc,out_embedding = model(inputs1.float(),inputs2.float())
     out_conv5_t0, out_conv5_t1 = out_conv5
     out_fc_t0,out_fc_t1 = out_fc
     out_embedding_t0,out_embedding_t1 = out_embedding
